db4c042466957ced9c9188dd90ad919a6b5fd722
Ade <ade.akingbade@citypantry.com>
2024-07-23 21:13:24 +0100

    Created initial API gateway dotnet project

---
+++ HotelAppLambda/HotelAppLambda/HotelApp.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace HotelAppLambda;
+
+public class HotelApp
+{
+    public APIGatewayProxyResponse AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var response = new APIGatewayProxyResponse()
+        {
+            Headers = new Dictionary<string, string>(),
+            StatusCode = 200
+        };
+        
+        response.Headers.Add("Access-Control-Allow-Origin", "*");
+        response.Headers.Add("Access-Control-Allow-Headers", "*");
+        response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS,POST");
+        
+        Console.WriteLine("Ade is Testing - OK");
+        
+        return response;
+    }
+}
