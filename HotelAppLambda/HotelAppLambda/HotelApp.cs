d64ed056bea8bc041b18782de52e0161a5a3af04
Ade <ade.akingbade@justeattakeaway.com>
2025-01-25 11:36:04 +0000

    Updated add hotel logic to push to S3 and dynamoDB

--- HotelAppLambda/HotelAppLambda/HotelApp.cs
+++ HotelAppLambda/HotelAppLambda/HotelApp.cs
@@ -1,5 +1,16 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
+using HotelAppLambda.Models;
+using HttpMultipartParser;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -7,7 +18,7 @@ namespace HotelAppLambda;
 
 public class HotelApp
 {
-    public APIGatewayProxyResponse AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
     {
         var response = new APIGatewayProxyResponse()
         {
@@ -18,9 +29,73 @@ public class HotelApp
         response.Headers.Add("Access-Control-Allow-Origin", "*");
         response.Headers.Add("Access-Control-Allow-Headers", "*");
         response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS,POST");
+
+        var bodyContent = request.IsBase64Encoded
+            ? Convert.FromBase64String(request.Body)
+            : Encoding.UTF8.GetBytes(request.Body);
         
-        Console.WriteLine("Ade is Testing - OK");
+        using var memStream = new MemoryStream(bodyContent);
+        var formData = MultipartFormDataParser.Parse(memStream);
         
+        var hotelName = formData.GetParameterValue("hotelName");
+        var hotelRating = formData.GetParameterValue("hotelRating");
+        var hotelCity = formData.GetParameterValue("hotelCity");
+        var hotelPrice = formData.GetParameterValue("hotelPrice");
+
+        var file = formData.Files.FirstOrDefault();
+        var fileName = (file.Name) + '_' + DateTime.Now.ToFileTime();
+        await using var fileContentStream = new MemoryStream();
+        await file.Data.CopyToAsync(fileContentStream);
+        fileContentStream.Position = 0; 
+        
+        var encodedIdToken = request.Headers["Authorization"];
+        var token = encodedIdToken.StartsWith("Bearer ") ? encodedIdToken.Substring("Bearer ".Length) : encodedIdToken;
+        var idToken = new JwtSecurityToken(token);
+        var group = idToken.Claims.FirstOrDefault(x => x.Type == "cognito:groups");
+        var usernameClaim = idToken.Claims.FirstOrDefault(c => c.Type == "cognito:username");
+        var username = usernameClaim?.Value == null ? "defaultUsername" : usernameClaim.Value;
+        
+        if (group == null || group.Value != "Admin")
+        {
+            response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            response.Body = JsonSerializer.Serialize(new { Error = "Unauthorized. Must be a member of Admin group." });
+        }
+        
+        var region = Environment.GetEnvironmentVariable("AWS_REGION");
+        var bucketName = Environment.GetEnvironmentVariable("bucketName");
+
+        var client = new AmazonS3Client(RegionEndpoint.GetBySystemName(region));
+        var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(region));
+        try
+        {
+            await client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = fileName,
+                InputStream = fileContentStream,
+                AutoCloseStream = true,
+            });
+            
+            var hotel = new Hotel
+            {
+                UserId = username,
+                Id = Guid.NewGuid().ToString(),
+                Name = hotelName,
+                CityName = hotelCity,
+                Price = int.Parse(hotelPrice),
+                Rating = int.Parse(hotelRating),
+                FileName = fileName
+            };
+            
+            using var dbContext = new DynamoDBContext(dbClient);
+            await dbContext.SaveAsync(hotel);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
         return response;
     }
 }
